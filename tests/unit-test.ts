63646a47a09704607a348d17a95ec6c4f849dc0b
Dani Tseiltin <39850374+danitseitlin@users.noreply.github.com>
2020-06-23 00:46:47 +0300

    Adding the option to pass over headers (#4)
    * Adding the option to pass over headers
    
    * Code review changes

--- tests/unit-test.ts
+++ tests/unit-test.ts
@@ -33,10 +33,12 @@ describe('Mock server testing', async function() {
             email: 'user2@gmail.com'
         });
         response = await httpClient.get('/users');
+        expect(response.headers.cookie).to.equal('my-key', 'The cookie of the response')
         expect(response.data.length).eql(2, 'Users count');
         response = await httpClient.get('/users/1');
         expect(response.data.name).eql('user1', 'User name');
         expect(response.data.email).eql('user1@gmail.com', 'User email address');
+        expect(response.headers.cookie).not.to.equal('my-key', 'The cookie of the response')
         await httpClient.put('/users/1', {
             name: 'user1-updated',
             email: 'user1-updated@gmail.com'
